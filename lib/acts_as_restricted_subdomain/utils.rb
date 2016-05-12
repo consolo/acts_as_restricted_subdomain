97de705fd7533b9af17d5fba309c5a9e94a9a27b
Andrew Coleman <penguincoder@gmail.com>
2016-05-12 14:16:39 -0400

    syntax fix

--- lib/acts_as_restricted_subdomain/utils.rb
+++ lib/acts_as_restricted_subdomain/utils.rb
@@ -7,7 +7,7 @@ module RestrictedSubdomain
     private
     
     def subdomain_from_host(host)
-      host.split(SUBDOMAIN_REGEX).first.to_s.gsub(/[^A-Za-z0-9\-\_/, '')
+      host.split(SUBDOMAIN_REGEX).first.to_s.gsub(/[^A-Za-z0-9\-\_/], '')
     end
   end
 end
